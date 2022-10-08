b95b0229fd23776e690e417c523f2bf119e5ec58
unknowIfGuestInDream <tang97155@163.com>
2022-10-08 18:50:50 +0800

    feat: 英语日语文档初始化

--- docs/.vuepress/sidebar/en.ts
+++ docs/.vuepress/sidebar/en.ts
@@ -1,4 +1,6 @@
 import { sidebar } from "vuepress-theme-hope";
+import {comsol, software, vscode} from "./software";
+import {code} from "./code";
 
 export const enSidebar = sidebar({
   "/en/": [
@@ -19,4 +21,49 @@ export const enSidebar = sidebar({
     "intro",
     "slides",
   ],
+  "/en/software/vscode/": vscode,
+  "/en/software/git/": "structure",
+  "/en/software/comsol/": comsol,
+  "/en/software/": software,
+  "/en/code/windows/": [
+    "",
+    "shortcut-key",
+    "hidden-file",
+    "add-path",
+    "cmd",
+    "notepad",
+  ],
+  // "/code/website/jquery/": jquery,
+  //
+  // "/code/website/html/": html,
+  //
+  // "/code/website/css/": css,
+  //
+  // "/code/website/": website,
+  //
+  // "/code/vue/": vue,
+
+  "/en/code/node-js/": "structure",
+
+  // "/code/mini-app/": miniapp,
+  //
+  // "/code/language/typescript/": typescript,
+  //
+  // "/code/language/python/": python,
+  //
+  // "/code/language/markdown/": markdown,
+  //
+  // "/code/language/js/": javascript,
+  //
+  // "/code/language/dart/": dart,
+  //
+  // "/code/language/": language,
+  //
+  // "/code/github/": github,
+  //
+  // "/code/basic/": basic,
+
+  "/en/code/Android/": ["", "intro", "base", "resource"],
+
+  "/en/code/": code,
 });
---
+++ docs/en/code/language/C/README.md
@@ -0,0 +1,13 @@
+---
+title: C 语言教程
+icon: c
+category: C
+---
+
+::: tip
+
+正在完善中...
+
+:::
+
+<!-- more -->
