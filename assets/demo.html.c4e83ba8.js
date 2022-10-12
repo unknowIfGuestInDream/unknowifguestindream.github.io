19dffd745a4f609fa58714e04e8319ac70c47c8f
unknowIfGuestInDream <tang97155@163.com>
2022-10-12 14:31:39 +0000

    Deploying to gh-pages from @ unknowIfGuestInDream/unknowifguestindream.github.io@815a903e3476e88b764712370d3b2199123f0d32 🚀

---
+++ assets/demo.html.c4e83ba8.js
@@ -0,0 +1,84 @@
+import{_ as n,a as s}from"./card-intro.541044af.js";import{_ as a}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as e,c as t,f as l}from"./app.963e91c8.js";const p={},i=l('<h2 id="\u8D26\u6237\u9875\u9762" tabindex="-1"><a class="header-anchor" href="#\u8D26\u6237\u9875\u9762" aria-hidden="true">#</a> \u8D26\u6237\u9875\u9762</h2><p><img src="'+n+`" alt="\u8D26\u6237\u9875\u9762\u4E8C\u7EF4\u7801" loading="lazy"></p><div class="language-yaml ext-yml line-numbers-mode"><pre class="language-yaml"><code><span class="token key atrule">title</span><span class="token punctuation">:</span> \u7EDF\u4E00\u8EAB\u4EFD\u8BA4\u8BC1
+<span class="token key atrule">grey</span><span class="token punctuation">:</span> <span class="token boolean important">true</span>
+<span class="token key atrule">content</span><span class="token punctuation">:</span>
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> text
+    <span class="token key atrule">type</span><span class="token punctuation">:</span> p
+    <span class="token key atrule">heading</span><span class="token punctuation">:</span> \u7EDF\u4E00\u8EAB\u4EFD\u8BA4\u8BC1\u4ECB\u7ECD
+    <span class="token key atrule">text</span><span class="token punctuation">:</span>
+      <span class="token punctuation">-</span> \u5B66\u6821\u4E3A\u6BCF\u4E2A\u4EBA\u5206\u914D\u4E86\u4E00\u4E2A\u7EDF\u4E00\u8EAB\u4EFD\uFF0C\u8FD9\u4E2A\u8EAB\u4EFD\u5C31\u662F\u60A8\u7684\u90AE\u7BB1\u8D26\u53F7\u548C\u5BC6\u7801\u3002
+      <span class="token punctuation">-</span> \u65E0\u8BBA\u60A8\u767B\u5F55\u6821\u56ED\u7F51\u3001\u8FDB\u5165\u5185\u7F51\u67E5\u770B\u901A\u77E5\u3001\u67E5\u770B\u662F\u5426\u6B20\u4E66\u672A\u8FD8\u3001\u67E5\u770B\u6821\u56ED\u5361\u4F59\u989D\u4EE5\u53CA\u6D88\u8D39\u8BB0\u5F55\u3001\u8FDB\u5165\u672C\u79D1\u751F\u6216\u7814\u7A76\u751F\u9009\u8BFE\u5E73\u53F0\u3001\u67E5\u6210\u7EE9\u3001\u67E5\u8BFE\u8868\u3001\u8BBF\u95EE BB \u5E73\u53F0\u3001\u672C\u79D1\u6559\u5B66\u670D\u52A1\u7CFB\u7EDF\u7B49\u7B49\uFF0C\u5404\u79CD\u4E0E\u4FE1\u606F\u67E5\u8BE2\u63D0\u4EA4\u7684\u6709\u5173\u7684\u64CD\u4F5C\u90FD\u9700\u8981\u8FD9\u4E2A\u7EDF\u4E00\u8EAB\u4EFD\u3002
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> list
+    <span class="token key atrule">header</span><span class="token punctuation">:</span> \u4E86\u89E3\u66F4\u591A
+    <span class="token key atrule">content</span><span class="token punctuation">:</span>
+      <span class="token punctuation">-</span> <span class="token key atrule">text</span><span class="token punctuation">:</span> \u7EDF\u4E00\u8EAB\u4EFD\u8BA4\u8BC1
+        <span class="token key atrule">icon</span><span class="token punctuation">:</span> apply
+        <span class="token key atrule">path</span><span class="token punctuation">:</span> authorize
+
+      <span class="token punctuation">-</span> <span class="token key atrule">text</span><span class="token punctuation">:</span> \u6821\u56ED\u90AE\u7BB1
+        <span class="token key atrule">icon</span><span class="token punctuation">:</span> email
+        <span class="token key atrule">path</span><span class="token punctuation">:</span> mail
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> footer
+    <span class="token key atrule">author</span><span class="token punctuation">:</span> Mr.Hope
+    <span class="token key atrule">time</span><span class="token punctuation">:</span> 2020\u5E747\u670817\u65E5
+
+<span class="token key atrule">shareable</span><span class="token punctuation">:</span> <span class="token boolean important">true</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="\u6821\u56ED\u5361\u4ECB\u7ECD" tabindex="-1"><a class="header-anchor" href="#\u6821\u56ED\u5361\u4ECB\u7ECD" aria-hidden="true">#</a> \u6821\u56ED\u5361\u4ECB\u7ECD</h2><p><img src="`+s+`" alt="\u6821\u56ED\u5361\u4ECB\u7ECD\u9875\u9762\u4E8C\u7EF4\u7801" loading="lazy"></p><div class="language-yaml ext-yml line-numbers-mode"><pre class="language-yaml"><code><span class="token key atrule">title</span><span class="token punctuation">:</span> \u6821\u56ED\u5361\u4ECB\u7ECD
+<span class="token key atrule">author</span><span class="token punctuation">:</span> Mr.Hope
+<span class="token key atrule">date</span><span class="token punctuation">:</span> 2020\u5E747\u670818\u65E5
+<span class="token key atrule">shareable</span><span class="token punctuation">:</span> <span class="token boolean important">true</span>
+<span class="token key atrule">content</span><span class="token punctuation">:</span>
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> title
+    <span class="token key atrule">text</span><span class="token punctuation">:</span> \u6821\u56ED\u5361\u8BF4\u660E
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> text
+    <span class="token key atrule">type</span><span class="token punctuation">:</span> p
+    <span class="token key atrule">text</span><span class="token punctuation">:</span> \u6821\u56ED\u5361\u662F\u60A8\u5728\u4E1C\u5E08\u6821\u56ED\u5185\u7684\u901A\u7528\u6D88\u8D39\u5361\u4EE5\u53CA\u8EAB\u4EFD\u8BA4\u8BC1\u5361\uFF0C\u662F\u60A8\u5728\u4E1C\u5E08\u7684\u65E5\u5E38\u751F\u6D3B\u4E0E\u5B66\u4E60\u4E2D\u662F\u975E\u5E38\u91CD\u8981\u7684\u5B58\u5728\uFF0C\u4E00\u65E6\u5FD8\u8BB0\u643A\u5E26\u6216\u4E22\u5931\u6821\u56ED\u5361\u4F1A\u9020\u6210\u8BF8\u591A\u4E0D\u4FBF\u3002
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> img
+    <span class="token key atrule">src</span><span class="token punctuation">:</span> https<span class="token punctuation">:</span>//mp.innenu.com/img/card/card.jpg
+    <span class="token key atrule">desc</span><span class="token punctuation">:</span> \u6821\u56ED\u5361
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> title
+    <span class="token key atrule">text</span><span class="token punctuation">:</span> \u4E3B\u8981\u529F\u80FD
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> text
+    <span class="token key atrule">type</span><span class="token punctuation">:</span> ul
+    <span class="token key atrule">heading</span><span class="token punctuation">:</span> \u751F\u6D3B
+    <span class="token key atrule">text</span><span class="token punctuation">:</span>
+      <span class="token punctuation">-</span> \u5BBF\u820D\u7F51\u7EDC\u7684\u81EA\u52A9\u4ED8\u8D39
+      <span class="token punctuation">-</span> \u98DF\u5802\u6D88\u8D39
+      <span class="token punctuation">-</span> \u8D85\u5E02\u8D2D\u7269
+      <span class="token punctuation">-</span> \u6D74\u6C60\u6D17\u6D74
+      <span class="token punctuation">-</span> \u6D17\u6F31\u95F4\u7684\u751F\u6D3B\u7528\u6C34
+      <span class="token punctuation">-</span> \u996E\u7528\u6C34\u63A5\u53D6
+      <span class="token punctuation">-</span> \u4F53\u80B2\u9986\u95E8\u7981
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> text
+    <span class="token key atrule">type</span><span class="token punctuation">:</span> ul
+    <span class="token key atrule">heading</span><span class="token punctuation">:</span> \u5B66\u4E60
+    <span class="token key atrule">text</span><span class="token punctuation">:</span>
+      <span class="token punctuation">-</span> \u56FE\u4E66\u8D85\u671F\u7F5A\u6B3E
+      <span class="token punctuation">-</span> \u56FE\u4E66\u9986\u7535\u5B50\u9605\u89C8\u5BA4\u7684\u56FE\u4E66\u501F\u9605
+      <span class="token punctuation">-</span> \u56FE\u4E66\u9986\u95E8\u7981
+      <span class="token punctuation">-</span> \u671F\u4E2D\u671F\u672B\u8003\u8BD5\u8EAB\u4EFD\u8BA4\u8BC1
+      <span class="token punctuation">-</span> \u8BA1\u7B97\u673A\u7B49\u7EA7\u8003\u8BD5\u8EAB\u4EFD\u8BA4\u8BC1
+      <span class="token punctuation">-</span> \u82F1\u8BED\u56DB\u516D\u7EA7\u8003\u8BD5\u8EAB\u4EFD\u8BA4\u8BC1
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> title
+    <span class="token key atrule">text</span><span class="token punctuation">:</span> \u6D88\u8D39\u9650\u989D
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> text
+    <span class="token key atrule">type</span><span class="token punctuation">:</span> p
+    <span class="token key atrule">text</span><span class="token punctuation">:</span>
+      <span class="token punctuation">-</span> <span class="token string">&quot;\u7531\u4E8E\u6821\u56ED\u5361\u662F\u4E1C\u5E08\u5185\u90E8\u7684\u201C\u7535\u5B50\u94B1\u5305\u201D\uFF0C\u5B66\u6821\u4E3A\u4E86\u4FDD\u969C\u60A8\u7684\u8D44\u91D1\u5B89\u5168\uFF0C\u5BF9\u6821\u56ED\u5361\u8FDB\u884C\u4E86\u6D88\u8D39\u4E0A\u7684\u9650\u5236\u3002\u6BCF\u5F20\u6821\u56ED\u5361\u7684\u521D\u59CB\u6D88\u8D39\u8BBE\u7F6E\u4E3A: \u5355\u6B21\u6D88\u8D39\u9650\u989D\u4E3A 20 \u5143\uFF0C\u6BCF\u65E5\u7D2F\u8BA1\u6D88\u8D39\u9650\u989D\u4E3A 100 \u5143\u3002&quot;</span>
+      <span class="token punctuation">-</span> \u5982\u679C\u5F53\u65E5\u60A8\u7684\u6D88\u8D39\u91D1\u989D\u8D85\u8FC7\u9650\u989D\uFF0C\u5219\u5728\u5237\u5361\u65F6\uFF0C\u60A8\u9700\u8981\u5728\u5237\u5361\u673A\u4E0A\u8F93\u5165\u5BC6\u7801\u624D\u80FD\u5B8C\u6210\u6D88\u8D39\u3002
+      <span class="token punctuation">-</span> \u5F53\u7136\uFF0C\u60A8\u53EF\u4EE5\u6839\u636E\u81EA\u8EAB\u7684\u6D88\u8D39\u4E60\u60EF\u901A\u8FC7\u81EA\u52A9\u7EC8\u7AEF\u673A\u4FEE\u6539\u6D88\u8D39\u9650\u989D\uFF0C\u5E76\u5B9A\u671F\u66F4\u6539\u6821\u56ED\u5361\u5BC6\u7801\u3002
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> title
+    <span class="token key atrule">text</span><span class="token punctuation">:</span> \u4E0D\u540C\u7684\u8BA1\u8D39\u7CFB\u7EDF
+
+  <span class="token punctuation">-</span> <span class="token key atrule">tag</span><span class="token punctuation">:</span> text
+    <span class="token key atrule">text</span><span class="token punctuation">:</span> \u76F4\u996E\u673A\u3001\u751F\u6D3B\u7528\u6C34\u3001\u7F51\u8D39\u90FD\u662F\u6709\u81EA\u5DF1\u7684\u5C0F\u94B1\u5305\u7684\uFF0C\u800C\u4E14\u53EF\u4EE5\u901A\u8FC7\u6821\u56ED\u5361\u5145\u503C\u3002\u540C\u65F6\u7535\u8D39\u548C\u6D74\u6C60(\u672C\u90E8)\u4E5F\u662F\u6709\u81EA\u5DF1\u7684\u94B1\u5305\u7684\u3002
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),c=[i];function u(o,d){return e(),t("div",null,c)}const m=a(p,[["render",u],["__file","demo.html.vue"]]);export{m as default};
